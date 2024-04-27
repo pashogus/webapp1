c1847856afff21772bf409823ac168a93fb41ab2
pashogus <43311430+pashogus@users.noreply.github.com>
2024-04-27 21:18:03 +0530

    Update GreetingController.java

--- src/main/java/com/example/restservice/greeting/GreetingController.java
+++ src/main/java/com/example/restservice/greeting/GreetingController.java
@@ -18,7 +18,7 @@ public class GreetingController {
 	}
 
 	@GetMapping("/greetingTest")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public Greeting greetingTest(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 
